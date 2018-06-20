8a742eeadc47ca7fd7e2fc844aca86c1ab9282ea
Administrator <Administrator@USER-20180504PG>
2018-06-20 22:58:45 +0800

    yyyyyyhyu

--- www.zz.com/src/main/java/com/zz/fault/controller/FaultController.java
+++ www.zz.com/src/main/java/com/zz/fault/controller/FaultController.java
@@ -1,10 +1,9 @@
 package com.zz.fault.controller;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -26,6 +25,7 @@ import com.zz.core.shiro.token.manager.TokenManager;
 import com.zz.epu.service.EpuService;
 import com.zz.user.service.UUserService;
 
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 @Controller
@@ -61,16 +61,27 @@ public class FaultController extends BaseController {
 	}
 	
 	@RequestMapping(value="init")
-	public ModelAndView init(ModelMap map){	
+	public ModelAndView init(ModelMap modelMap){	
 		
-		ModelAndView modelAndView = new  ModelAndView("woodare/fault/index");
+		ModelAndView modelAndView = new  ModelAndView("fault/index");
 		modelAndView.addObject("leftMenuview", "7");//显示左侧菜单 0 个人中心 1用户中心 2 权限管理 3用电曲线数据 4设备管理 5实时监控7故障定位
 		UUser token =  userService.selectByPrimaryKey(TokenManager.getToken().getId());
 		modelAndView.addObject("token", token);//左侧上方管理员信息
-		modelAndView.addObject("falutReasonMap", this.getFalutReason());
-		modelAndView.addObject("isFalutMap", this.getIsFalut());
-		modelAndView.addObject("isCancelledMap", this.isCancelled());
-		
+		Map<String, Object> falutReasonMap  = this.getFalutReason();
+		modelAndView.addObject("falutReasonMap", falutReasonMap);
+//		modelAndView.addObject("isFalutMap", this.getIsFalut());
+//		modelAndView.addObject("isCancelledMap", this.isCancelled());
+		Set<String> kedySet = falutReasonMap.keySet();
+		JSONArray jsonArrayFalutReason = new JSONArray();
+		for (String key : kedySet) {
+			JSONObject json = new JSONObject();
+			json.put("key", key);
+			json.put("value", falutReasonMap.get(key));
+			jsonArrayFalutReason.add(json);
+		}
+		modelMap.put("jsonArrayFalutReason", jsonArrayFalutReason);
+//		modelMap.put("isFalutMap", this.getIsFalut());
+//		modelMap.put("isCancelledMap", this.isCancelled());
 		return modelAndView;
 	}
 	
