f300134c989c900439a564f577fda38da11c5e68
ninedragon <shi_jin@hoperun.com>
2018-07-05 11:51:40 +0800

    代码提交

--- www.zz.com/src/main/java/com/zz/analysisAndDisplay/observer/Watcher.java
+++
@@ -1,8 +0,0 @@
-package com.zz.analysisAndDisplay.observer;
-
-public class Watcher implements java.util.Observer {
-  public void update(java.util.Observable obj, Object arg) {     
-    System.out.println("Update() called, count is "      
-                                + ((Integer) arg).intValue());     
-}     
-}    
