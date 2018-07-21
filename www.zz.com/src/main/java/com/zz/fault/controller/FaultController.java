01c2086e140684da29ff5524f6279328012099a1
Administrator <Administrator@192.168.0.111>
2018-07-21 11:37:26 +0800

    ffff

--- www.zz.com/src/main/java/com/zz/fault/controller/FaultController.java
+++ www.zz.com/src/main/java/com/zz/fault/controller/FaultController.java
@@ -55,7 +55,7 @@ public class FaultController extends BaseController {
 		ModelAndView modelAndView = new  ModelAndView("fault/index");
 		modelAndView.addObject("leftMenuview", "7");//显示左侧菜单 0 个人中心 1用户中心 2 权限管理 3用电曲线数据 4设备管理 5实时监控7故障定位
 		UUser token =  userService.selectByPrimaryKey(TokenManager.getToken().getId());
-		modelAndView.addObject("token", token);//左侧上方管理员信息
+		modelAndView.addObject("token", token);//左侧上方管理员信息 
 //		wsMessageService.sendToAllTerminal(token.getId()+"","{\"name\":\"falutNews\",\"key\":\"\"}");
 //		wsMessageService.sendToAllTerminal(token.getId()+"","{\"name\":\"faultRendering\",\"key\":\"8bfa662e-f506-4d8c-8d4a-912a6b61dd06\"}");
 		return modelAndView;
